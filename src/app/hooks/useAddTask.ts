4c257ed9303ea12722014e5c7486e292f7824393
Josip Celic <josipcelic22@gmail.com>
2026-09-15 18:20:42 +0200

    feat: add optimistic update for new tasks

--- src/app/hooks/useAddTask.ts
+++ src/app/hooks/useAddTask.ts
@@ -1,12 +1,30 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { addTask } from '../api/tasksApi';
+import { Task } from '../types/task';
 
 export const useAddTask = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: addTask,
-    onSuccess: () => {
+    onMutate: async (newTask) => {
+      const optimisticTask: Task = {
+        id: crypto.randomUUID(),
+        ...newTask,
+      };
+      await queryClient.cancelQueries({ queryKey: ['tasks'] });
+      const previous = queryClient.getQueryData(['tasks']);
+
+      queryClient.setQueryData<Task[]>(['tasks'], (old) => [
+        ...(old ?? []),
+        optimisticTask,
+      ]);
+      return { previous };
+    },
+    onError: (err, newTask, context) => {
+      queryClient.setQueryData(['tasks'], context?.previous);
+    },
+    onSettled: () => {
       queryClient.invalidateQueries({ queryKey: ['tasks'] });
     },
   });
